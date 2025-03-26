63ebbc0ac3a343102450a5ce812e614db4510173
João Augusto <joaomafra199@gmail.com>
2025-03-26 20:01:34 -0300

    Commit da calculadora angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'calculadora';
+
+  links = [
+    { title: 'Explore the Docs', link: 'https://angular.dev' },
+    { title: 'Learn with Tutorials', link: 'https://angular.dev/tutorials' },
+    { title: 'CLI Docs', link: 'https://angular.dev/tools/cli' },
+    { title: 'Angular Language Service', link: 'https://angular.dev/tools/language-service' },
+    { title: 'Angular DevTools', link: 'https://angular.dev/tools/devtools' },
+  ];
+}
